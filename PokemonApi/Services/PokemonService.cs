abfe00e8526158057d2dc7c42eda181c08255358
DamianGuardian <eliasnieto953@gmail.com>
2025-02-11 23:30:14 -0600

    actualizacion
    tema nuevo

---
+++ PokemonApi/Services/PokemonService.cs
@@ -0,0 +1,12 @@
+using PokemonApi.Dtos;
+
+namespace PokemonApi.Services;
+
+public class PokemonService : IPokemonService
+{
+    public Task<PokemonResponseDto> GetPokemonById(Guid id, CancellationToken cancellationToken){
+        
+        return null;
+
+    }
+}
