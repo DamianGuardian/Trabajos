71e8c8c078ce3421b8f87f393e0ea1b2a3aaf28d
DamianGuardian <eliasnieto953@gmail.com>
2025-03-10 15:26:27 -0600

    trabajo
    miércoles avance

---
+++ pokedex_api/internal/service/pokemon.go/pokemon.go
@@ -0,0 +1,17 @@
+package service
+
+type PokemonService interface {
+GetPokemonById(id string) (*model.Pokemon, error)
+}
+
+type pokemonService struct {
+
+}
+
+func NewPokemonService() PokemonService {
+	return &pokemonService{}
+}
+
+func (s *pokemonService) GetPokemonById(id string) (*model.Pokemon, error) {
+	
+}
