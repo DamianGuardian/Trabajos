62622b543b524d20b28bf8a02a5bed29180d3228
DamianGuardian <eliasnieto953@gmail.com>
2025-03-24 15:31:52 -0600

    s

--- PokedexApi/Repositories/IHobbiesRepository.cs
+++ PokedexApi/Repositories/IHobbiesRepository.cs
@@ -8,5 +8,8 @@ public interface IHobbiesRepository
     // Método para obtener un hobby por su ID
     Task<Hobbies> GetHobbyBYIdAsync(int id, CancellationToken cancellationToken);
     Task <List<Hobbies>> GetHobbiesByNameAsync (string name, CancellationToken cancellationToken);
+    
+    // Método para eliminar un hobby por su ID
+    Task<bool> DeleteHobbyByIdAsync(int id, CancellationToken cancellationToken);
 
 }
