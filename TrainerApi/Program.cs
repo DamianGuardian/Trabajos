5483a2604bc0a22842e184e394c15c9d2b484f9b
DamianGuardian <eliasnieto953@gmail.com>
2025-05-15 17:13:10 -0600

    apunte

--- TrainerApi/Program.cs
+++ TrainerApi/Program.cs
@@ -1,5 +1,6 @@
 using MongoDB.Driver;
 using TrainerApi.Infrastructure;
+using TrainerApi.Repositories;
 using TrainerApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,9 +9,11 @@ builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("Mo
 builder.Services.AddSingleton<IMongoDatabase>(sp =>
 {
     var settings = builder.Configuration.GetSection("MongoDB").Get<MongoDBSettings>();
-    var client = new MongoClient(settings.DatabaseName);
+    var client = new MongoClient(settings.ConnectionString);
     return client.GetDatabase(settings.DatabaseName);
 });
+
+builder.Services.AddScoped<ITrainerRepository, TrainerRepository>();
 
 // Add services to the container.
 builder.Services.AddGrpc();
